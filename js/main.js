6786abe4a936abdf4fb09e4dbb3e5d2084d05936
yosko99 <thedriftmaniaka1997@gmail.com>
2024-12-31 15:40:29 +0200

    init

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const menuToggle = document.querySelector('.header__menu-toggle');
+const nav = document.querySelector('.header__nav');
+
+menuToggle.addEventListener('click', function (e) {
+    nav.classList.toggle('active');
+    e.stopPropagation();
+});
+
+document.addEventListener('click', function (e) {
+    if (!nav.contains(e.target) && !menuToggle.contains(e.target)) {
+        nav.classList.remove('active');
+    }
+});
