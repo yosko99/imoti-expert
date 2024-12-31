481b3ef3b3061c1e105defd3b8d840086df2783d
yosko99 <thedriftmaniaka1997@gmail.com>
2024-12-31 23:10:51 +0200

    responsive and seo updates

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
-const menuToggle = document.querySelector('.header__menu-toggle');
-const nav = document.querySelector('.header__nav');
+const handleNavigation = () => {
+    const menuToggle = document.querySelector('.header__menu-toggle');
+    const nav = document.querySelector('.header__nav');
 
-menuToggle.addEventListener('click', function (e) {
-    nav.classList.toggle('active');
-    e.stopPropagation();
-});
+    menuToggle.addEventListener('click', function (e) {
+        nav.classList.toggle('active');
+        e.stopPropagation();
+    });
 
-document.addEventListener('click', function (e) {
-    if (!nav.contains(e.target) && !menuToggle.contains(e.target)) {
-        nav.classList.remove('active');
-    }
-});
+    document.addEventListener('click', function (e) {
+        if (!nav.contains(e.target) && !menuToggle.contains(e.target)) {
+            nav.classList.remove('active');
+        }
+    });
+}
+
+handleNavigation();
